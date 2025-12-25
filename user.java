c3f3234516b62ebb6f7f50b05e34f13c47b9d30f
Arman IT <armanhu0786@gmail.com>
2025-12-25 11:30:33 +0530

    git commit -m 'new update for loop'

--- user.java
+++ user.java
@@ -3,5 +3,11 @@ class User{
   public static void main(String args[]){
   
        System.out.println("this one is the first");
+       for(int i=0;i<=5;i++){
+        for(int j=0;j<=i;j++){
+          System.out.print(" *");
+        }
+        System.out.println();
+       }
+     }
   }
-}
