eace69cf277e4469007d9704237e9d85fe701d2a
Arman IT <armanhu0786@gmail.com>
2025-12-25 17:08:38 +0530

    add subtract

--- user.java
+++ user.java
@@ -1,6 +1,13 @@
 
 class User{
+
+  // gira ticket no 102
+  public static void findSubtract(int a,int b){
+    int subtract=a-b;
+    System.out.println(subtract);
+  }
   public static void main(String args[]){
+    findSubtract(20, 10);
   
        System.out.println("this one is the first");
        for(int i=0;i<=5;i++){
