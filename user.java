a722c039ab9165ace004718c1d4f29abb51e207e
Arman IT <armanhu0786@gmail.com>
2025-12-25 20:17:41 +0530

    add-new-syntex

--- user.java
+++ user.java
@@ -1,27 +1,21 @@
 
 class User{
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
   // gira ticket no 102
   public static void findSubtract(int a,int b){
     int subtract=a-b;
     System.out.println(subtract);
-=======
+  }
   public static void FindSum(int a,int b){
     int sum=a+b;
     System.out.println(sum);
->>>>>>> Stashed changes
-=======
-  public static void FindSum(int a,int b){
-    int sum=a+b;
-    System.out.println(sum);
->>>>>>> Stashed changes
+    System.out.println("this one is sum");
   }
   public static void main(String args[]){
     findSubtract(20, 10);
   
-    FindSum(2, 04);
+    FindSum(2, 4);
        System.out.println("this one is the first");
        for(int i=0;i<=5;i++){
         for(int j=0;j<=i;j++){
